690a2218742cfad60b25a9e662a5b4ea1ce91295
abhinav002p <abhinavprakash0511@gmail.com>
2025-03-25 19:34:00 +0530

    -

--- hr.js
+++
@@ -1,6 +0,0 @@
- let istudent="abhinav";
-  console.log(istudent);
-  istudent="23";
-  console.log(istudent);
-  istudent=true;
-console.log(istudent);
---
+++ hr1.js
@@ -0,0 +1,4 @@
+let age=19;
+let canvote=(age>18);
+
+console.log(canvote);
