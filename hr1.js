b4dacb33c5fd7e4e1d002a76fd47586a94cf16f5
abhinav002p <abhinavprakash0511@gmail.com>
2025-03-26 01:05:31 +0530

    -

--- hr1.js
+++ hr1.js
@@ -7,3 +7,36 @@ let ans1=sum(2,4);
 
 console.log(ans);
 console.log(ans1);
+
+
+
+
+//second example
+function sub(a,b){
+    let totalsub=a-b;
+    return totalsub;
+}
+let ans3=sub(9,2)
+let ans4=sub(10,2)
+console.log(ans3);
+console.log(ans4);
+
+//third example
+function mul(a,b){
+    let totalmul=a * b;
+    return totalmul;
+}
+let ans5=mul(7,9);
+let ans6=mul(8,9);
+
+console.log(ans5);
+console.log(ans6);
+//forth example
+function div(a,b){
+    let totaldiv=a/b;
+    return totaldiv;
+}
+let ans7=div(10,2)
+let ans8=div(21,3)
+console.log(ans7);
+console.log(ans8);
