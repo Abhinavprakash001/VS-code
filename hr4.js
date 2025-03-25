fa642e10db0d8bd8a2cb5d8db6b6d583aad1817a
abhinav002p <abhinavprakash0511@gmail.com>
2025-03-26 02:18:25 +0530

    i

---
+++ hr4.js
@@ -0,0 +1,32 @@
+let user2name="rahul"
+let user2age="23"
+let user2sex="male"
+
+let user2={
+      name:"rahul",
+      age:"23",
+      sex:"male"
+}
+
+
+console.log(user2.name)
+console.log(user2.age)
+console.log(user2.sex)
+
+
+let user1= {
+    name:"abhinav",
+    age :"23",
+    sex :"male"
+}
+
+
+
+
+
+
+
+console.log(user1["age"])
+
+console.log(user1["name"])
+console.log(user1["sex"]);
