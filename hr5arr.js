67d61ef785c003e5d47008fa1c556c7fbfe7675b
abhinav002p <abhinavprakash0511@gmail.com>
2025-03-26 21:15:41 +0530

    l

---
+++ hr5arr.js
@@ -0,0 +1,10 @@
+let arr=["abhinav","age",{
+    name:"abhinav",
+    age:"23",
+    cities:["banglore","delhi","patna",{
+        country:"india",
+    cities:"dehrdun"}] 
+}];
+
+
+console.log(arr[2].cities[3].cities);
