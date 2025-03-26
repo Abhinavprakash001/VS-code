17b8ece1bdec77415f4d20d983a10ad03df58169
abhinav002p <abhinavprakash0511@gmail.com>
2025-03-26 18:37:34 +0530

    3

---
+++ hr6.js
@@ -0,0 +1,20 @@
+function greet(user){
+    console.log("hi " + user.name + " your age is" + user.age);
+}
+let user={
+    name:"abhinav",
+    age:"23",
+    adress:"chhawani"
+}
+
+
+
+function greet(user1){
+    console.log("my adress is "+user1.adress)
+}
+let user1={
+    adress:"Chhawani"
+}
+
+greet(user1)
+greet(user)
