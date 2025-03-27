e2092b2f973ea4a52a25a74ef13026d869d169db
abhinav002p <abhinavprakash0511@gmail.com>
2025-03-27 22:29:03 +0530

    8

---
+++ hr8.js
@@ -0,0 +1,7 @@
+function sum(a,b){
+    return parseInt(a)+parseInt(b);
+}
+let ans=sum("20",30)
+console.log(ans)
+
+// one to n number
---
+++ hr9.js
@@ -0,0 +1,10 @@
+function sum(n){
+    let ans1=0;
+    for(let i=1; i<=n; i++){
+     ans1=ans1+i;
+
+    }
+    return ans1;
+}
+let ans1=sum(100)
+console.log(ans1)
